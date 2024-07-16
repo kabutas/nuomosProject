ed569bbc87e4f04e4541c2f98de3da608807d3f4
AndriusKarpis <kabutas@gmail.com>
2024-07-16 12:11:01 +0300

    removed unused path in urls
    TO DO:
    
    user reservation details view
    
    redo reservation page and logic -
    
    staff page         +-
    
    MAYBE:
    
    add pay method???
    
    TO FIX:
    - car detail returns button to car list, not to previous page.
    
    -Design need fixes --------

--- motionRent/urls.py
+++ motionRent/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, temporary, RentalItemListView, RentalItemDetailView, RentalCreateView, LocationDetailView, \
+from .views import home, RentalItemListView, RentalItemDetailView, RentalCreateView, LocationDetailView, \
     register, login_view, logout_view, about, all_rentals, rental_detail, rental_update, rental_delete
 
 urlpatterns = [
     path('', home, name='home'),
-    path('temporary/', temporary, name='temporary'),
     path('rental-items/', RentalItemListView.as_view(), name='rental_items_list'),
     path('rental-items/<int:pk>/', RentalItemDetailView.as_view(), name='rental_item_detail'),
     path('create-rental/<int:pk>/', RentalCreateView.as_view(), name='create_rental'),
